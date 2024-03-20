2e93191f1d00c8eb1f70c826e2e73e25e894654c
Alpay Akçer <info@alpayakcer.com>
2024-03-20 13:54:30 +0300

    IActionResult kullanımı

--- WebApiTest.RestApi/Controllers/HotelController.cs
+++ WebApiTest.RestApi/Controllers/HotelController.cs
@@ -15,34 +15,83 @@ namespace WebApiTest.RestApi.Controllers
             _hotelService = hotelService;
         }
 
+        //[HttpGet]
+        //public List<Hotel> GetList()
+        //{
+        //    return _hotelService.TGetAllHotel();
+        //}
+
+        //[HttpGet("{id}")]
+        //public Hotel GetList(int id)
+        //{
+        //    return _hotelService.TGetHotelById(id);
+        //}
+
+        //[HttpPost]
+        //public Hotel Post([FromBody] Hotel hotel)
+        //{
+        //    return _hotelService.TCreateHotel(hotel);
+        //}
+
+        //[HttpPut]
+        //public Hotel Update([FromBody] Hotel hotel)
+        //{
+        //    return _hotelService.TUpdateHotel(hotel);
+        //}
+
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //    _hotelService.TDeleteHotel(id);
+        //}
         [HttpGet]
-        public List<Hotel> GetList()
+        public IActionResult GetList()
         {
-            return _hotelService.TGetAllHotel();
+            var hotels = _hotelService.TGetAllHotel();
+            return Ok(hotels);
         }
 
         [HttpGet("{id}")]
-        public Hotel GetList(int id)
+        public IActionResult GetList(int id)
         {
-            return _hotelService.TGetHotelById(id);
+            var hotel = _hotelService.TGetHotelById(id);
+            if (hotel != null)
+            {
+                return Ok(hotel);
+            }
+            return NotFound();
         }
 
         [HttpPost]
-        public Hotel Post([FromBody] Hotel hotel)
+        public IActionResult Post([FromBody] Hotel hotel)
         {
-            return _hotelService.TCreateHotel(hotel);
+            if (ModelState.IsValid)
+            {
+                var createHotel = _hotelService.TCreateHotel(hotel);
+                return CreatedAtAction("Get", new { id = createHotel.ID }, createHotel);
+            }
+            return BadRequest(ModelState);
         }
 
         [HttpPut]
-        public Hotel Update([FromBody] Hotel hotel)
+        public IActionResult Update([FromBody] Hotel hotel)
         {
-            return _hotelService.TUpdateHotel(hotel);
+            if (_hotelService.TGetHotelById(hotel.ID) != null)
+            {
+                return Ok(_hotelService.TUpdateHotel(hotel)); //200 + Data
+            }
+            return NotFound();
         }
 
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
-            _hotelService.TDeleteHotel(id);
+            if (_hotelService.TGetHotelById(id) != null)
+            {
+                _hotelService.TDeleteHotel(id);
+                return Ok(); //200
+            }
+            return NotFound(); //404
         }
     }
 }
